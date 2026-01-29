e1bf875d8cf18cdb2e69b05772cb9b146691c968
erwindW99 <erwindwiranata17@gmail.com>
2026-01-29 18:58:47 +0700

    Rumah Reptile 1st upload

---
+++ components/database.js
@@ -0,0 +1,95 @@
+// console.log("hello world!");
+
+let users = [
+  {
+    id: 1,
+    email: "admin123@gmail.com",
+    password: "12345678",
+  },
+];
+
+let database = [
+  {
+    id: 1,
+    nama: "Gecko",
+    stok: 5,
+    harga: 200_000,
+    foto: "../images/gecko.jpg",
+    deskripsi:
+      "Reptil kecil yang aktif di malam hari dan mampu memanjat dinding berkat bantalan lengket di kakinya.",
+  },
+  {
+    id: 2,
+    nama: "Iguana",
+    stok: 2,
+    harga: 6_000_000,
+    foto: "../images/iguana.jpg",
+    deskripsi:
+      "Reptil herbivora berukuran besar yang dikenal tenang dan sering berjemur untuk mengatur suhu tubuhnya.",
+  },
+  {
+    id: 3,
+    nama: "Ular Python",
+    stok: 1,
+    harga: 200_000,
+    foto: "../images/ularPython.jpg",
+    deskripsi:
+      "Ular besar yang tidak berbisa dan membunuh mangsanya dengan cara melilit sebelum menelan secara utuh.",
+  },
+  {
+    id: 4,
+    nama: "Dinosaurus",
+    stok: 5,
+    harga: 450_000_000_000,
+    foto: "../images/dinosaurus.jpg",
+    deskripsi:
+      "Reptil purba yang hidup jutaan tahun lalu dan kini hanya dapat dipelajari melalui fosil dan penelitian ilmiah.",
+  },
+  {
+    id: 5,
+    nama: "Kura-kura",
+    stok: 2,
+    harga: 6_500_000,
+    foto: "../images/kura-kura.jpeg",
+    deskripsi:
+      "Reptil bercangkang keras yang bergerak lambat dan memiliki umur hidup yang sangat panjang.",
+  },
+  {
+    id: 6,
+    nama: "Kadal Gurun",
+    harga: 4_500_000,
+    stok: 1,
+    foto: "../images/kadalGurun.jpg",
+    deskripsi:
+      "Kadal yang hidup di daerah panas dan kering dengan kemampuan beradaptasi terhadap suhu ekstrem.",
+  },
+  {
+    id: 7,
+    nama: "Komodo",
+    stok: 5,
+    harga: 256_000,
+    foto: "../images/komodo.jpg",
+    deskripsi:
+      "Kadal terbesar di dunia yang berasal dari Indonesia dan dikenal sebagai predator kuat dengan gigitan berbahaya.",
+  },
+  {
+    id: 8,
+    nama: "Bunglon",
+    stok: 2,
+    harga: 20_000,
+    foto: "../images/bunglon.jpg",
+    deskripsi:
+      "Reptil unik yang dapat mengubah warna kulitnya untuk kamuflase dan komunikasi.",
+  },
+  {
+    id: 9,
+    nama: "Tokek",
+    stok: 1,
+    harga: 6_000,
+    foto: "../images/tokek.jpg",
+    deskripsi:
+      "Reptil nokturnal yang dikenal dengan suara khasnya dan kemampuan menempel kuat di permukaan licin.",
+  },
+];
+
+//deskripsi nya nanti statis aja
---
+++ components/unload.js
@@ -0,0 +1,11 @@
+function loadComponent(id, file) {
+  fetch(file)
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById(id).innerHTML = data;
+    })
+    .catch((err) => console.error("Failed load:", file, err));
+}
+
+loadComponent("navbar", "../components/navbar.html");
+loadComponent("footer", "../components/footer.html");
